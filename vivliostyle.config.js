c603cfba82c40e856e382fb9a222185d4b097436
xiupos <me@xiupos.net>
2023-05-08 09:44:30 +0900

    記事の追加

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -3,11 +3,29 @@ module.exports = {
   author: '北大言語学サークル Huling <info@huling.org>',
   language: 'ja',
   // size: 'A4', // paper size.
-  theme: './theme', // .css or local dir or npm package. default to undefined.
+  theme: './theme',
   entry: [
-    'manuscript.md',
-  ], // `entry` can be `string` or `object` if there's only single markdown file.
-  entryContext: './src', // default to '.' (relative to `vivliostyle.config.js`).
+    'preface.md', // はじめに
+
+    //
+    // 第3外国語概説
+    // 
+    '3gai/chu.md', // 古教会スラブ語
+    '3gai/ain.md', // アイヌ語
+    // '3gai/ara.md', // アラビア語
+    '3gai/fin.md', // フィンランド語
+    '3gai/pol.md', // ポーランド語
+    // '3gai/tur.md', // トルコ語
+
+    //
+    // 寄稿
+    //
+    'contri/consti.md', // 日本国憲法の言語学的解析
+    'contri/edu_jpn.md', // 知っていますか?「日本語教育」
+    // 'contri/chara.md', // 文字
+    // 'contri/fra_rus.md', // ロシア国内でのフランス語
+  ],
+  entryContext: './src',
   output: [ // path to generate draft file(s). default to '{title}.pdf'
     './dist/output.pdf', // the output format will be inferred from the name.
     {
